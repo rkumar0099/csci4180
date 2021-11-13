688396f40f1c8e25c67c3acdfe478abc019d1201
root <root@RK.localdomain>
2021-11-13 13:50:05 +0500

    added specific Edge type for more readability and understandability

--- AdjacencyList.java
+++ AdjacencyList.java
@@ -11,25 +11,20 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-public class AdjacencyList implements Writable  
-  {	private List<IntWritable> data; //list size is multiple of 2, i%2 = 0 for node id and i%2 for node distance.   
-  	private IntWritable size;
+public class AdjacencyList implements Writable {
+    private List<Edge> data; //list to store Edge for a given vertex v
+    private IntWritable size;
 
-    public AdjacencyList()
-    {
-      //System.out.println("Creating List!");
-      this.data = new ArrayList <IntWritable>();
-      this.size = new IntWritable(0); 
+    public AdjacencyList() {
+      this.data = new ArrayList<Edge>();
+      this.size = new IntWritable(0);
     }
 
-    public AdjacencyList(int cap)
-    {
-      //System.out.println("Creating List!");
-      this.data = new ArrayList <IntWritable>(cap);
-      this.size = new IntWritable(0); 
+    public AdjacencyList(int cap) {
+      this.data = new ArrayList<Edge>(cap);
+      this.size = new IntWritable(0);
     }
-
-
+    
     public int getIntSize(){
     	return this.size.get();
     }
@@ -38,96 +33,50 @@ public class AdjacencyList implements Writable
     	return this.size;
     }
 
-    public int getIntValue(int index){
-        return data.get(index).get();
+    public Edge getEdge(int index) {
+      return this.data.get(index)
     }
 
-    public IntWritable getValue(int index){
-    	return data.get(index);
+    public void add(Edge e) {
+      this.data.add(e);
+      this.size.set(this.size.get() + 1);
     }
 
-    public void addData(int nodeid, int distance )
-    {
-    	data.add(new IntWritable(nodeid));
-    	data.add(new IntWritable(distance));
-    	size.set(size.get()+2);
-    
+    public void merge(AdjacencyList list) {
+      int listSize = list.getIntSize();
+      this.size.set(this.size.get() + listSize);
+
+      for (int i = 0; i < listSize; i++) {
+        this.data.add(list.getEdge(i));
+      }
+
     }
 
-    public void addData(IntWritable nodeid, IntWritable distance )
-    {
-    	data.add(nodeid);
-    	data.add(distance);
-    	size.set(size.get()+2);
-    
+    public void write(DataOutput output) {
+      this.size.write(output);
+      for (Edge e: this.data) {
+        e.write(output);
+      }
     }
 
-    public void merge(AdjacencyList list)
-    {   int listSize = list.getIntSize();
-    	
-    	size.set(size.get()+listSize);
-    	
-    	for(int i = 0; i < listSize; i++)
-    	{
-    		data.add(list.getValue(i));
-    	}
-    }
-    
-    @Override
-     //Must overwrite to as writable is an abstract class.
-    public void write(DataOutput out)  throws IOException{
-    	
+    public void readFields(DataInput input) {
+      this.size = new IntWritable(0);
+      this.size.readFields(input);
 
-    	 
-    	int size = this.size.get();
-        this.size.write(out);
-        for(int i = 0; i < size; i+=2 )
-    	{
-
-    		this.data.get(i).write(out);
-    		this.data.get(i+1).write(out);
-    	//	System.out.println("Mapper writing:" +this.data.get(i).get() + " " + this.data.get(i+1).get());
-    	}
-
-    //	toReturn +=	toReturn += "(" + data.get(size-2) + " " +  data.get(size-1) + ")";
+      this.data = new ArrayList<Edge>();
+      for (int i = 0; i < this.size.get(); i++) {
+        Edge e = new Edge();
+        e.readFields(input);
+        this.data.add(e);
+      }
     }
 
-      @Override
-    //overriding default readFields method. 
-    //It de-serializes the byte stream data
-    //Must overwrite to as writable is an abstract class.
-    public void readFields(DataInput in) throws IOException {
-      	
-      	this.data = new ArrayList <IntWritable>();
-      	this.size = new IntWritable(0); 
-        this.size.readFields(in);
-
-        int size = this.size.get();
-        for(int i = 0; i < size; i+=2 )
-    	{    
-    		IntWritable nodeId = new IntWritable();
-    	    IntWritable distance = new IntWritable();
-    		nodeId.readFields(in);
-    		distance.readFields(in);
-    		this.data.add(nodeId);
-    		this.data.add(distance);
-    	//	System.out.println("Reducer reading:" + nodeId.get() + " " + distance.get());
-    	}
+    public String toString() {
+      String s = "Size: " + Integer.valthis.size.get() + "\n";
+      for (Edge e: this.data) {
+        s += e.toString();
+      }
+      return s;
     }
-    //if your output format is Text or if you use print for this type, the to string method will be invoked. 
-    public String toString(){
-    	int size = this.size.get();
-    	String toReturn = "";
-    	for(int i = 0; i < size-2; i+=2 )
-    	{
 
-    		toReturn += "(" + data.get(i) + " " +  data.get(i+1) + "), ";
-
-    	}
-        if(size >= 2){
-            toReturn += "(" + data.get(size-2) + " " +  data.get(size-1) + ")";
-        }
-    
-    	return toReturn; 
-    }
 }
---
+++ Edge.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.util.*;
+import java.io.DataOutput;
+import java.io.DataInput;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class Edge implements Writable {
+    private IntWritable vertex;
+    private IntWritable cost;
+
+    public Edge() {}
+
+    public Edge(int v, int c) {
+        this.vertex = new IntWritable(v);
+        this.cost = new IntWritable(c);
+    }
+
+    public Edge(IntWritable v, IntWritable c) {
+        this.vertex = v;
+        this.cost = c;
+    }
+
+    public IntWritable getVertex() {
+        return this.vertex;
+    }
+
+    public IntWritable getCost() {
+        return this.cost;
+    }
+
+    public int getIntVertex() {
+        return this.vertex.get();
+    }
+
+    public int getIntCost() {
+        return this.cost.get();
+    }
+
+    public void write(DataOutput output) throws IOException {
+        this.vertex.write(output);
+        this.cost.write(output);
+    }
+
+    public void readFields(DataInput input) throws IOException {
+        this.vertex = new IntWritable(0);
+        this.cost = new IntWritable(0);
+
+        this.vertex.readFields(input);
+        this.cost.readFields(input);
+    }
+
+    public String toString() {
+        return this.vertex.get() + " " + this.cost.get();
+    }
+
+}
