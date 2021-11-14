94eaf155271a052e6b97c8ca6ad9bc071ab4b3c5
root <root@RK.localdomain>
2021-11-14 15:40:57 +0500

    shifted files from main dir to rabindar dir

--- run.sh
+++
@@ -1,5 +0,0 @@
-hadoop fs -rm -r /a2/output || echo ""
-javac -classpath `yarn classpath` PDPreProcess.java AdjacencyList.java Tuple.java
-jar -cvf pdpreprocess.jar *.class
-hadoop jar pdpreprocess.jar PDPreProcess /a2/input /a2/output
-#hadoop fs -cat /a2/output/part-r-00000
