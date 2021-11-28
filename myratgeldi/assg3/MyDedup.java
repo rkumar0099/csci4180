5116df726c83c64fdb36371da8765553c0570216
Myratgeldi99 <myratgeldi.j.99@gmail.com>
2021-11-28 14:56:20 +0800

    edited mydedup

--- myratgeldi/assg3/MyDedup.java
+++ myratgeldi/assg3/MyDedup.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.lang.Math;
+import java.lang.System;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,25 +14,26 @@ public class MyDedup {
         return n != 0 && ((n & (n - 1)) == 0);
     }
     
-    static int getK(int avgChunkSize) {
+    static int getAnchorMask(int avgChunkSize) {
         int count = 0;
- 
+
         while(avgChunkSize != 0)
         {
             avgChunkSize >>= 1;
             count += 1;
         }
         // since avgChunkSize is power of two
-        return count - 1;
+        count -= 1;
+        return (1 << count) - 1;
     }
 
 
     public static void main(String args[]) {
         int a = Integer.parseInt(args[0]);
-        int k = getK(a);
-        System.out.println("my pow: " + k);
-        // System.out.println("pow of two: " + checkPowerTwo(a));
-        System.out.println((1 << k) - 1);
+        int mask = getAnchorMask(a);
+        System.out.println("my num: " + a);
+        System.out.println("pow of two: " + checkPowerTwo(a));
+        System.out.println("mask: " + mask);
     }
 
     public static void main2(String args[]) {
@@ -50,40 +52,84 @@ public class MyDedup {
             d = Integer.parseInt(args[4]);
             uploadFile = args[5];
             storage = args[6];
-            byte[] container = new byte[1024];
             try {
                 InputStream input = new FileInputStream(uploadFile);
-                int k = getK(avgChunkSize);
-                // chunking
-                int index = 0;
-                int off = 0;
+                int mask = getAnchorMask(avgChunkSize);
+
+                int containerOffset = 0;
+                int chunkOffset = 0;
+                byte[] container = new byte[1024];
                 byte[] chunk = new byte[maxChunkSize];
-                // first read from input
-                int fread = input.read(chunk, off, minChunkSize);
-                int mask = (1 << k) - 1;
-                if (fread == -1) {
-                    // empty file
-                }
-                else {
-                    if (fread < minChunkSize) {
-                        // file size smaller than minChunkSize
-                        off = off + fread;
-                    }
-                    else {
-                        off = off + minChunkSize;
-                        int curRFP = 0;
-                        for (int i = 0; i < minChunkSize; i++) {
-                            curRFP = (curRFP + (chunk[i] % avgChunkSize * (int) Math.pow(d, minChunkSize - i - 1) % avgChunkSize) % avgChunkSize) % avgChunkSize;
-                        }
-                        if (curRFP & mask == 0) {
-                            
-                        }
-                        else {
+                boolean finishedChunking = false;
+                boolean newChunk = true;
+                int curRFP = 0;
+                while (!finishedChunking) {
+                    if (newChunk) {
+                        int numBytesRead = input.read(chunk, chunkOffset, minChunkSize);
+                        if (numBytesRead != -1) {chunkOffset += numBytesRead;}
+                        if (numBytesRead == -1 || numBytesRead < minChunkSize) {
+                            // eof reached
+                            finishedChunking = true;
+                            // move chunk into container
+                            System.arraycopy(chunk, 0, container, containerOffset, chunkOffset);
+                            containerOffset += chunkOffset;
+                            chunk = new byte[maxChunkSize];
+                            chunkOffset = 0;
 
                         }
+                        else {
+                            // first time computing RFP for this chunk
+                            for (int i = chunkOffset - minChunkSize; i < chunkOffset; i++) {
+                                curRFP = (curRFP + (chunk[i] % avgChunkSize * (int) Math.pow(d, minChunkSize - i - 1) % avgChunkSize) % avgChunkSize) % avgChunkSize;
+                            }
+                            if ((curRFP & mask) == 0) {
+                                // move chunk into container
+                                System.arraycopy(chunk, 0, container, containerOffset, chunkOffset);
+                                containerOffset += chunkOffset;
+                                chunk = new byte[maxChunkSize];
+                                chunkOffset = 0;
+                            }
+                            else {
+                                newChunk = false;
+                            }
+                        }
+                        
+                    }
+                    else {
+                        if (chunkOffset + 1 > maxChunkSize) {
+                            // move chunk into container
+                            System.arraycopy(chunk, 0, container, containerOffset, chunkOffset);
+                            containerOffset += chunkOffset;
+                            chunk = new byte[maxChunkSize];
+                            chunkOffset = 0;
+                        }
+                        else {
+                            int cur = input.read();
+                            if (cur == -1) {
+                                // eof reached
+                                finishedChunking = true;
+                                // move chunk into container
+                                System.arraycopy(chunk, 0, container, containerOffset, chunkOffset);
+                                containerOffset += chunkOffset;
+                                chunk = new byte[maxChunkSize];
+                                chunkOffset = 0;
+                            }
+                            else {
+                                chunk[chunkOffset] = cur;
+                                chunkOffset++;
+                                curRFP = (d * (curRFP - (((int) Math.pow(d, minChunkSize - 1)) % avgChunkSize * chunk[chunkOffset - minChunkSize])
+                                % avgChunkSize) % avgChunkSize + chunk[chunkOffset - 1]) % avgChunkSize;
+                                if ((curRFP & mask) == 0) {
+                                    // move chunk into container
+                                    System.arraycopy(chunk, 0, container, containerOffset, chunkOffset);
+                                    containerOffset += chunkOffset;
+                                    chunk = new byte[maxChunkSize];
+                                    chunkOffset = 0;
+                                }
+                            }
+                        }
                     }
                 }
-                
 
             } catch (IOException e) {
                 System.out.println("An error occurred.");
